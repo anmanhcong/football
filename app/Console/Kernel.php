9a54c89ae2e65976ba15c344356044087ec57604
congam <congam@ghtk.vn>
2021-02-08 13:21:04 +0700

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('scrap:scrapMatch')->everyMinute();;
+         $schedule->command('scrap:scrapMatch')->twiceDaily(6, 18);
     }
 
     /**
