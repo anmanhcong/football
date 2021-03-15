3e0e6a17dc0f8d5c9257a8baee60a96466f32d83
congam <congam@ghtk.vn>
2021-03-15 21:53:51 +0700

    fix

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -12,11 +12,57 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
 
 </head>
 <body class="layout-wrapper ">
-    <div class="container-fluid">
-
+    <div class="container">
+       <div class="wrapper">
+           <div class="page-left">
+               <div class="logo">
+                   <span class="logo-live">LIVE</span>
+                   <span class="logo-football">FOOTBALL</span>
+               </div>
+               <div class="ball">
+                   <div class="ball-line">
+                       <img src="{{asset('image/ball.png')}}" class="google-play">
+                       <span class="ball-text">GOAL ALERTS</span>
+                   </div>
+                   <div class="ball-line">
+                       <img src="{{asset('image/ball.png')}}" class="google-play">
+                       <span class="ball-text">MATCH STATISTICS</span>
+                   </div>
+                   <div class="ball-line">
+                       <img src="{{asset('image/ball.png')}}" class="google-play">
+                       <span class="ball-text"> HEAD-TO-HEAD DATA</span>
+                   </div>
+                   <div class="ball-line">
+                       <img src="{{asset('image/ball.png')}}" class="google-play">
+                       <span class="ball-text">LIVE LEAGUE TABLES</span>
+                   </div>
+                   <div class="ball-line">
+                       <img src="{{asset('image/ball.png')}}" class="google-play">
+                       <span class="ball-text">BREAKING NEWS</span>
+                   </div>
+                   <div class="ball-line">
+                       <img src="{{asset('image/ball.png')}}" class="google-play">
+                       <span class="ball-text">DOWNLOAD WALLPAPERS</span>
+                   </div>
+               </div>
+           </div>
+           <div class="right position-relative">
+               <div class="store">
+                   <img src="{{asset('image/google.png')}}" class="google-play">
+                   <img src="{{asset('image/app_store.png')}}" class="app_store">
+               </div>
+              <div class="device">
+                  <img src="{{asset('image/team.png')}}" class="team">
+                  <img src="{{asset('image/mobile.png')}}" class="mobile">
+                  <img src="{{asset('image/shoot.png')}}" class="shoot">
+              </div>
+           </div>
+       </div>
     </div>
 </body>
 </html>
