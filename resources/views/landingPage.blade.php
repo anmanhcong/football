6a9a371729c43c808c67623b64416aec3c1fdac0
congam <congam@ghtk.vn>
2021-03-17 19:00:48 +0700

    fxi

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -7,10 +7,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Landing Page</title>
     <!-- Scripts -->
-    <script src="https://raw.githubusercontent.com/anmanhcong/football/master/public/js/app.js" defer></script>
+    <script src="http://livefootball.top/static/js/app.js" defer></script>
 
     <!-- Styles -->
-    <link href="https://raw.githubusercontent.com/anmanhcong/football/master/public/css/app.css" rel="stylesheet">
+    <link href="http://livefootball.top/static/css/app.css" rel="stylesheet">
     <link href="http://livefootball.top/static/css/style.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
@@ -26,40 +26,41 @@
                </div>
                <div class="ball">
                    <div class="ball-line">
-                       <img src="{{asset('image/ball.png')}}" class="google-play">
+                       <img src="http://livefootball.top/static/media/image/ball.png" class="google-play">
                        <span class="ball-text">GOAL ALERTS</span>
                    </div>
                    <div class="ball-line">
-                       <img src="{{asset('image/ball.png')}}" class="google-play">
+                       <img src="http://livefootball.top/static/media/image/ball.png" class="google-play">
                        <span class="ball-text">MATCH STATISTICS</span>
                    </div>
                    <div class="ball-line">
-                       <img src="{{asset('image/ball.png')}}" class="google-play">
+                       <img src="http://livefootball.top/static/media/image/ball.png" class="google-play">
                        <span class="ball-text"> HEAD-TO-HEAD DATA</span>
                    </div>
                    <div class="ball-line">
-                       <img src="{{asset('image/ball.png')}}" class="google-play">
+                       <img src="http://livefootball.top/static/media/image/ball.png" class="google-play">
                        <span class="ball-text">LIVE LEAGUE TABLES</span>
                    </div>
                    <div class="ball-line">
-                       <img src="{{asset('image/ball.png')}}" class="google-play">
+                       <img src="http://livefootball.top/static/media/image/ball.png" class="google-play">
                        <span class="ball-text">BREAKING NEWS</span>
                    </div>
                    <div class="ball-line">
-                       <img src="{{asset('image/ball.png')}}" class="google-play">
+                       <img src="http://livefootball.top/static/media/image/ball.png" class="google-play">
                        <span class="ball-text">DOWNLOAD WALLPAPERS</span>
                    </div>
                </div>
            </div>
            <div class="right position-relative">
                <div class="store">
-                   <img src="{{asset('image/google.png')}}" class="google-play">
-                   <img src="{{asset('image/app_store.png')}}" class="app_store">
+
+                   <img src="http://livefootball.top/static/media/image/google.png" class="google-play">
+                   <img src="http://livefootball.top/static/media/image/app_store.png" class="app_store">
                </div>
               <div class="device">
-                  <img src="{{asset('image/team.png')}}" class="team">
-                  <img src="{{asset('image/mobile.png')}}" class="mobile">
-                  <img src="{{asset('image/shoot.png')}}" class="shoot">
+                  <img src="http://livefootball.top/static/media/image/team.png" class="team">
+                  <img src="http://livefootball.top/static/media/image/mobile.png" class="mobile">
+                  <img src="http://livefootball.top/static/media/image/shoot.png" class="shoot">
               </div>
            </div>
        </div>
